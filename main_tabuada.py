c157e34063922f46cade3e2610ecb227a181715a
Felipe Moura <felipemouracardoso52@gmail.com>
2026-03-28 01:25:15 -0300

    Remove academic grade calculation from main_tabuada.py

--- main_tabuada.py
+++ main_tabuada.py
@@ -9,16 +9,3 @@ for i in range(1, 11):
     print(f"{num} x {i} = {resultado}")
 
 print("Fim do programa\n")
-
-#Somando notas academicas
-total_notas = 0
-qtd_provas = 4
-
-for i in range(1, qtd_provas + 1):
-    nota = float(input(f"Digite a nota da prova {i}: "))
-    total_notas = total_notas + nota # Acumulador
-
-media = total_notas / qtd_provas
-print(f"\nA média final do semestre é: {media}")
-
-print("Fim do programa.")
